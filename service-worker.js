804efd1e4fc91f3891c7d18e7db40a4646e629ce
y1040054307-cell <y1040054307@gmail.com>
2026-08-01 16:03:56 +0800

    完善植物枯萎与复苏机制

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sunny-english-island-v3";
+const CACHE_NAME = "sunny-english-island-v4";
 const APP_FILES = ["./", "./index.html", "./styles.css", "./curriculum.js", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
